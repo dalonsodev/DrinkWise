48ee587577530bbe2f7138051d470de86f168245
dalonso <masdavidalonso@gmail.com>
2025-10-31 20:14:31 +0100

    refactor(css): include index.css with imports and media queries
    docs: update README accordingly

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,7 @@ import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./i18n"
 
-import "./styles/globals.css"
-import "./styles/layout.css"
-import "./styles/components.css"
-import "./styles/pages.css"
-import "./styles/utils.css"
+import "./styles/index.css"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(<App />)
